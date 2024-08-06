ac68c38628b1a090ef340e42f61d6e83a3b557d4
Randy Jorge Aguilera <ijarndev@gmail.com>
2024-08-06 17:30:06 -0400

    Minor changes

--- src/components/OptionFrame.jsx
+++ src/components/OptionFrame.jsx
@@ -6,7 +6,7 @@ export function OptionFrame({ options, title, category }) {
     <div className="flex flex-col w-full px-6">
       <h2 className="text-center text-4xl text-gray-700">{title}</h2>
 
-      <div className="flex justify-start sm:justify-center gap-14 sm:gap-20 mt-20 flex-row flex-wrap md:min-h-80">
+      <div className="flex justify-between px-10 sm:justify-center gap-x-10 gap-y-14 sm:gap-20 mt-20 flex-row flex-wrap md:min-h-80">
         {
           options.map(option => {
             return <Option  
